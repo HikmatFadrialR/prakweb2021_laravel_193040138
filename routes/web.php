8d8de215fae482382c0733ed81880f2e31bbca8e
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2021-10-08 13:46:22 +0700

    Mencoba fitur blade templating engine untuk mengelola tampilan halaman web pada laravel

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Hikmat Fadrial Ramadhan",
         "email" => "hikmatfadrial2000@gmail.com",
         "image" => "luffy.jpg"
@@ -26,5 +29,54 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Hikmat Fadrial Ramadhan",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque minima iste necessitatibus accusamus, numquam incidunt est accusantium provident deleniti dolores illo suscipit obcaecati animi, esse, veritatis officiis porro perspiciatis nobis? Consequatur iure dolorem asperiores dolore illo eum, repudiandae a, cupiditate ducimus nobis iste itaque voluptatum, consequuntur ea et nam aspernatur libero cumque eligendi ab perferendis quas? Illum iusto necessitatibus incidunt non laboriosam dignissimos blanditiis quidem quod obcaecati libero. Laborum saepe similique tempore deleniti numquam doloribus corporis, dolores veritatis voluptas accusantium."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Ilham",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quae debitis dolor iste, inventore accusantium repellendus, voluptatibus modi aliquam praesentium suscipit voluptates ducimus iusto id! Accusantium quam ab ratione voluptatibus at non mollitia sapiente iusto temporibus hic earum nobis quidem illo nam, iste vero dolor rerum perferendis totam, nihil facilis delectus fuga ducimus quos. Autem quaerat veritatis architecto unde laboriosam deserunt eligendi reprehenderit inventore. Deleniti, eligendi, totam mollitia quia debitis expedita quas ex voluptate quae iste illum ad sed nesciunt quam quasi quos. Odit magni est dolores placeat accusamus voluptatibus distinctio optio similique dignissimos molestiae, atque sint explicabo illum animi!"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman Single Post
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Hikmat Fadrial Ramadhan",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque minima iste necessitatibus accusamus, numquam incidunt est accusantium provident deleniti dolores illo suscipit obcaecati animi, esse, veritatis officiis porro perspiciatis nobis? Consequatur iure dolorem asperiores dolore illo eum, repudiandae a, cupiditate ducimus nobis iste itaque voluptatum, consequuntur ea et nam aspernatur libero cumque eligendi ab perferendis quas? Illum iusto necessitatibus incidunt non laboriosam dignissimos blanditiis quidem quod obcaecati libero. Laborum saepe similique tempore deleniti numquam doloribus corporis, dolores veritatis voluptas accusantium."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Ilham",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quae debitis dolor iste, inventore accusantium repellendus, voluptatibus modi aliquam praesentium suscipit voluptates ducimus iusto id! Accusantium quam ab ratione voluptatibus at non mollitia sapiente iusto temporibus hic earum nobis quidem illo nam, iste vero dolor rerum perferendis totam, nihil facilis delectus fuga ducimus quos. Autem quaerat veritatis architecto unde laboriosam deserunt eligendi reprehenderit inventore. Deleniti, eligendi, totam mollitia quia debitis expedita quas ex voluptate quae iste illum ad sed nesciunt quam quasi quos. Odit magni est dolores placeat accusamus voluptatibus distinctio optio similique dignissimos molestiae, atque sint explicabo illum animi!"
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
