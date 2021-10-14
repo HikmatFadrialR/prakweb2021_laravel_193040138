0af2c22f48edd8e4ec9183fefdfb14e06defacdc
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2021-10-14 20:53:47 +0700

    Membuat Model, Collection dan juga Controller yang sederhana

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,55 +31,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hikmat Fadrial Ramadhan",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque minima iste necessitatibus accusamus, numquam incidunt est accusantium provident deleniti dolores illo suscipit obcaecati animi, esse, veritatis officiis porro perspiciatis nobis? Consequatur iure dolorem asperiores dolore illo eum, repudiandae a, cupiditate ducimus nobis iste itaque voluptatum, consequuntur ea et nam aspernatur libero cumque eligendi ab perferendis quas? Illum iusto necessitatibus incidunt non laboriosam dignissimos blanditiis quidem quod obcaecati libero. Laborum saepe similique tempore deleniti numquam doloribus corporis, dolores veritatis voluptas accusantium."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ilham",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quae debitis dolor iste, inventore accusantium repellendus, voluptatibus modi aliquam praesentium suscipit voluptates ducimus iusto id! Accusantium quam ab ratione voluptatibus at non mollitia sapiente iusto temporibus hic earum nobis quidem illo nam, iste vero dolor rerum perferendis totam, nihil facilis delectus fuga ducimus quos. Autem quaerat veritatis architecto unde laboriosam deserunt eligendi reprehenderit inventore. Deleniti, eligendi, totam mollitia quia debitis expedita quas ex voluptate quae iste illum ad sed nesciunt quam quasi quos. Odit magni est dolores placeat accusamus voluptatibus distinctio optio similique dignissimos molestiae, atque sint explicabo illum animi!"
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// Halaman Single Post
-
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hikmat Fadrial Ramadhan",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque minima iste necessitatibus accusamus, numquam incidunt est accusantium provident deleniti dolores illo suscipit obcaecati animi, esse, veritatis officiis porro perspiciatis nobis? Consequatur iure dolorem asperiores dolore illo eum, repudiandae a, cupiditate ducimus nobis iste itaque voluptatum, consequuntur ea et nam aspernatur libero cumque eligendi ab perferendis quas? Illum iusto necessitatibus incidunt non laboriosam dignissimos blanditiis quidem quod obcaecati libero. Laborum saepe similique tempore deleniti numquam doloribus corporis, dolores veritatis voluptas accusantium."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ilham",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quae debitis dolor iste, inventore accusantium repellendus, voluptatibus modi aliquam praesentium suscipit voluptates ducimus iusto id! Accusantium quam ab ratione voluptatibus at non mollitia sapiente iusto temporibus hic earum nobis quidem illo nam, iste vero dolor rerum perferendis totam, nihil facilis delectus fuga ducimus quos. Autem quaerat veritatis architecto unde laboriosam deserunt eligendi reprehenderit inventore. Deleniti, eligendi, totam mollitia quia debitis expedita quas ex voluptate quae iste illum ad sed nesciunt quam quasi quos. Odit magni est dolores placeat accusamus voluptatibus distinctio optio similique dignissimos molestiae, atque sint explicabo illum animi!"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
