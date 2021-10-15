5613568c8a5f732453e33bdcce316fe7e94eddda
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2021-10-15 13:24:52 +0700

    Membuat Category untuk setiap postingan dalam blog, serta menerapkan eloquent relationship untuk mengelola relasi antar table

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
     <h1 class="mb-5">{{ $post->title }}</h1>
 
+    <p>By. Hikmat Fadrial Ramadhan in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
